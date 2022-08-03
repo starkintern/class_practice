272060b552af09e83fada967da27b36b6e9047e5
jacob <jsov886@gmail.com>
2022-08-03 16:44:11 -0400

    Adding more methods to the family class to show what we do in terms of helping out.

--- main.py
+++ main.py
@@ -12,21 +12,28 @@
 
 class Overton:
 
-    def __init__(self, first, age, career):
+    def __init__(self, first, age, career, help):
         self.first = first
         self.age = age
         self.career = career
+        self.help = help
 
     def job(self):
-        return f'Name: {self.first}. Employment: {self.career}'
+        return f"Name: {self.first}. Employment: {self.career}"
+
+    def helps_out(self):
+        return f"{self.first} helps out by {self.help}."
 
 
-overton_1 = Overton('Scot', '52', 'Harris Teeter')
-overton_2 = Overton('Melissa', "36", 'SalesForce')
-overton_3 = Overton('Jacob', '26', 'Hunting')
-overton_4 = Overton('Ella', '6', 'Big sister')
-overton_5 = Overton('Emily', '3', 'Big sister')
-overton_6 = Overton('Parky', '0.10', 'Learning to walk')
+overton_1 = Overton("Scot", "52", "Harris Teeter", "trimming tress")
+overton_2 = Overton("Melissa", "36", "SalesForce", "encouraging good behaviors")
+overton_3 = Overton("Jacob", "26", "Hunting", "mowing the lawn and vacuuming the house")
+overton_4 = Overton("Ella", "6", "Big sister", "putting her plate in the sink and cleaning up her toys")
+overton_5 = Overton("Emily", "3", "Big sister", "putting her plate in the sink and cleaning up her toys")
+overton_6 = Overton("Parky", "0.11", "Learning to walk", "smiling and lifting our spirits")
 
 print(overton_1.job())
 print(overton_2.job())
+
+print(overton_3.helps_out())
+print(overton_6.helps_out())
